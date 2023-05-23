ee9fa4cfc3b4614f6c9c2c7005ebaacd87e95938
taqiussa <taqiussa@gmail.com>
2023-05-24 06:03:20 +0700

    data induk santri

--- app/Http/Controllers/DataIndukSantriController.php
+++ app/Http/Controllers/DataIndukSantriController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class DataIndukSantriController extends Controller
 {
-    //
+    public function index()
+    {
+        return inertia(
+            'Pengurus/DataIndukSantri',
+            [
+                'listSantri' => User::where('nis', '!=', null)
+                    ->with([
+                        'biodata',
+                        'alamat'
+                    ])
+                    ->orderBy('name')
+                    ->get()
+            ]
+        );
+    }
 }
