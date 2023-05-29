78ef27a50c079783bb758ec500815697b5baa0cd
unknown <taqiussa>
2023-05-29 10:18:29 +0700

    atur kategori pembayaran

--- resources/js/Pages/Bendahara/AturKategoriPembayaran.jsx
+++ resources/js/Pages/Bendahara/AturKategoriPembayaran.jsx
@@ -1,17 +1,53 @@
+import PrimaryButton from '@/Components/PrimaryButton'
 import InputText from '@/Components/Sia/InputText'
+import Sweet from '@/Components/Sia/Sweet'
 import Tahun from '@/Components/Sia/Tahun'
 import AppLayout from '@/Layouts/AppLayout'
 import { Head, useForm } from '@inertiajs/react'
 import React from 'react'
+import { toast } from 'react-toastify'
 
 const AturKategoriPembayaran = () => {
 
-    const { data, setData } = useForm({
+    const { data, setData, post, errors, processing, delete: destroy } = useForm({
         nama: ''
     })
 
     const onHandleChange = (e) => {
         setData(e.target.name, e.target.value)
+    }
+
+    const submit = (e) => {
+        e.preventDefault()
+
+        post(route('atur-kategori-pembayaran.simpan'),
+            {
+                onSuccess: () => {
+                    toast.success('Berhasil Simpan Kategori Pembayaran')
+                    setData({ ...data })
+                }
+            })
+
+    }
+
+    const handleDelete = (id) => {
+
+        Sweet.fire({
+            title: 'Hapus',
+            text: 'Anda Yakin Menghapus ? ',
+            icon: 'question',
+            showCancelButton: true,
+            cancelButtonText: 'Batal',
+            confirmButtonText: 'Ya, Hapus!'
+        })
+            .then((result) => {
+                if (result.isConfirmed)
+                    destroy(route('atur-kategori-pembayaran.hapus', { id: id }), {
+                        onSuccess: () => {
+                            toast.success('Berhasil Hapus Kategori')
+                        }
+                    })
+            })
     }
 
     return (
@@ -20,15 +56,18 @@ const AturKategoriPembayaran = () => {
             <div className="font-bold text-lg text-center text-slate-600 uppercase border-b-2 border-emerald-500 mb-3 bg-emerald-200">
                 atur kategori pembayaran
             </div>
-            <div className='lg:grid lg:grid-cols-3 lg:gap-2 lg:space-y-0 grid grid-cols-2 gap-2 pb-2'>
-                <InputText
-                    id='nama'
-                    name='nama'
-                    label='kategori pembayaran'
-                    value={data.nama}
-                    handleChange={onHandleChange}
-                />
-            </div>
+            <form action="">
+                <div className='lg:grid lg:grid-cols-3 lg:gap-2 lg:space-y-0 grid grid-cols-2 gap-2 pb-2'>
+                    <InputText
+                        id='nama'
+                        name='nama'
+                        label='kategori pembayaran'
+                        value={data.nama}
+                        handleChange={onHandleChange}
+                    />
+                </div>
+                <PrimaryButton children='simpan' />
+            </form>
         </>
     )
 }
