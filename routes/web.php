a7e12f27aa0c99c6e4631bdf872255ae09e8f237
taqiussa <taqiussa@gmail.com>
2023-06-01 19:46:36 +0700

    assign siswa

--- routes/web.php
+++ routes/web.php
@@ -27,17 +27,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/set', function () {
-
-    $user = User::where('nis','!=', null)->get();
-
-    foreach ($user as $data)
-    {
-        $data->assignRole('Santri');
-    }
-
-});
-
 Route::get('/', function () {
     return inertia('Auth/Login');
 });
