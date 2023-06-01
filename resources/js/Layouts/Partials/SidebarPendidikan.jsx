2909455b125803193b692eb1bcfd6e3dd683a018
taqiussa <taqiussa@gmail.com>
2023-06-01 17:56:31 +0700

    input absensi

--- resources/js/Layouts/Partials/SidebarPendidikan.jsx
+++ resources/js/Layouts/Partials/SidebarPendidikan.jsx
@@ -5,7 +5,7 @@ const SidebarPendidikan = ({ closeSide }) => {
     return (
         <div className='py-1'>
             <div className='text-slate-600 font-bold'>
-                Bendahara
+                Pendidikan
             </div>
             <div>
                 <SidebarLink closeSide={closeSide} href={route('input-absensi')} active={route().current('input-absensi')} label='input absensi' />
